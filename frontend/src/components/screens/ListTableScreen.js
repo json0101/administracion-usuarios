0f28f655e569040eb0880c78c8a6d4fd571b4bbc
Jason Armando Hernandez Aguilar <jasonhernandez@MacBook-Pro-de-Jason.local>
2021-05-17 22:28:40 -0600

    Nuevos cambios

--- frontend/src/components/screens/ListTableScreen.js
+++ frontend/src/components/screens/ListTableScreen.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import {NavLink, useHistory} from 'react-router-dom';
+import {apiFetch} from '../../helpers/ApiFetch';
 
 const ListTableScreen = () => {
-    return (
-        <>
-          <h1>Listado de pantallas</h1>  
-        </>
-    )
+
+  const [screens, setScreens] = useState([]);
+  const [validationMessage, setValidationMessage] = useState("");
+
+  const history = useHistory();
+
+  useEffect(() => {
+      updateScreens();
+  }, []);
+
+  const updateScreens = () => {
+      apiFetch.getAuth("screen")
+      .then(data =>  {
+          console.log("data", data)
+          if(data?.ok) {
+              setScreens(data.screens);
+          }
+      })
+      .catch(error => {
+          setValidationMessage(error.message);
+      });
+  };
+
+  const handleEdit = (screen) => {
+      if(screen)
+          history.push('screens/update/'+screen.screen_id);
+  }
+
+  const handleDelete = (screen) => {
+      if(!screen) {
+          return;
+      }
+
+      apiFetch.deleteAuth(`screen/delete/${screen.screen_id}`)
+          .then(data => {
+              setValidationMessage(data.message);
+              if(data.ok) {
+                  updateScreens();
+              }
+          })
+  }
+
+  return (
+      <>
+          <div className='rigth-panel'>
+          <div>
+              <h1>Pantallas</h1>
+          </div>
+          <NavLink exact to='/screens/create'>Nueva Pantalla</NavLink>
+          <table className="table">
+              <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Pantalla Nombre</th>
+                      <th>Activo</th>
+                      <th>Fecha de Creación</th>
+                      <th></th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {
+                      screens.map(screen => {
+                          return (
+                              <tr key={screen.screen_id}>
+                                  <td>{screen.screen_id}</td>
+                                  <td>{screen.screen_description}</td>
+                                  <td>{screen.active+""}</td>
+                                  <td>{screen.creation_date}</td>
+                                  <td className="button-panel">
+                                      <button className="btn secondary" onClick={() => handleEdit(screen) }>Edit</button>
+                                      <button className="btn danger" onClick={() => handleDelete(screen) }>Delete</button>
+                                  </td>
+                              </tr>
+                          )
+                      })
+                  }
+              </tbody>
+          </table>
+          <label>{validationMessage}</label>
+      </div>
+      </>
+  )
 }
 
 export default ListTableScreen;
--- frontend/src/components/screens/UpdateScreen.js
+++ frontend/src/components/screens/UpdateScreen.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import {apiFetch} from '../../helpers/ApiFetch';
+import {useParams} from 'react-router-dom';
 
 const UpdateScreen = () => {
+    const [screen, setScreen] = useState({});
+    const [description, setDescription] = useState("");
+    const [aplications, setAplications] = useState([]);
+    const [active, setActive] = useState(false);
+    const [message, setMessage] = useState("");
+    const [aplicationId, setAplicationId] = useState(0);
+    const {id} = useParams();
+
+    useEffect(() => {
+        apiFetch.getAuth(`aplication/resume`)
+            .then(data => {
+                console.log(data);
+                setAplications(data.aplications);
+            });
+    }, []);
+
+    useEffect(() => {
+
+        if(!id) {
+            cleanScreen();
+            return;
+        }
+
+        apiFetch.getAuth(`screen/${id}`)
+            .then(data => {
+                if(data.ok) {
+                    setScreen(data.screen);
+                    setActive(data.screen.active);
+                    setDescription(data.screen.screen_description);
+                }
+            })
+    }, [id])
+
+    const handleCreateScreen = (e) => {
+        e.preventDefault();
+
+        if(!id) {
+            apiFetch.postAuth('screen/create', {
+                screen_description: description,
+                aplication_id: aplicationId,
+                active: active
+            })
+            .then(data => {
+                setMessage(data.message);            
+            })
+            .catch(error => {
+                setMessage(error.message);
+            });
+        }
+
+        if(id) {
+            apiFetch.putAuth("screen/update", 
+            {
+                screen_id: screen.screen_id,
+                screen_description: description,
+                active: active,
+                user_id_updated: 1
+            })
+            .then(data => {
+                setMessage(data.message);
+            });
+        }
+    };
+
+    const cleanScreen = () => {
+        setDescription("");
+        setActive(false);    
+    }
+
     return (
         <>
-            <h1>Pantallas</h1>
+            <div className="rigth-panel">
+                <h1>Crear nueva pantalla</h1>
+                <form onSubmit={handleCreateScreen}>
+                    <div className="form">
+                        <div className="form-field">
+                            <label htmlFor='screen_description'></label>
+                            <input type="text" id="screen_description" value={description} onChange={(e) => { setDescription(e.target.value);}}/>
+                        </div>
+
+                        <div className="form-field">
+                            <label>Aplicacion ID</label>
+                            <select value={aplicationId} onChange={(e) => { setAplicationId(e.target.value) }}>
+                                <option value="0">-----select-----</option>
+                                { 
+                                    aplications.map(a => {
+                                        return <option key={a.aplication_id} value={a.aplication_id}>{a.aplication_description}</option>;
+                                    })
+                                } 
+                            </select>
+                        </div>
+
+                        <div className="form-field">
+                            <label htmlFor="screen_active">Activo</label>
+                            <input type="checkbox" id="screen_active" checked={active} onChange={(e) => { setActive(e.target.checked); } }/>
+                        </div>
+
+                        <div><label>{message}</label></div>
+                    </div>
+
+                    <button className="btn primary" type="submit" onSubmit={handleCreateScreen}>Guardar</button>
+                </form>
+            </div>
         </>
     )
 }
